a9a32298f3bd4113fd7b6b272d594d420d37009d
JM <jdreher15@gmail.com>
2017-09-26 13:39:37 -0600

    server file

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+    , bodyParser = require("body-parser")
+    , cors = require("cors")
+    , app = module.exports = express()
+    , port = 3000
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+//connection to the front end
+app.use(express.static(__dirname + "./../public"));
+
+
+
+
+
+
+app.listen(PORT, () => {console.log(`I'm listening...${PORT}`)})
